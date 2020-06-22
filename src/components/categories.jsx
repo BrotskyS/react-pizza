cca69a34c1d33fbd8368331672d8fc45daf953c7
BrotskyS <brotkyenginner@gmail.com>
2020-06-22 15:53:08 +0300

    '1'

---
+++ src/components/categories.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Categories({items, onClick}) {
+    return(
+        <div className="categories">
+            <ul>
+                <li className="active">Все</li>
+                {items.map((name, index) =>
+                    (<li onClick={() => onClick(name)} key={`${name}_${index}`}>{name}</li>)
+                )}
+            </ul>
+        </div>
+    )
+}
