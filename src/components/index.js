83737c6b1bb8a0e7f89f97ae30a1bca18b3ed52b
SergiyBrotsky <BrotskyS>
2020-07-08 14:02:56 +0300

    5

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-import Button from "./Button";
-import Header from "./Header";
-import Categories from "./categories";
-
-export {Button, Categories, Header};
+export { default as Button } from './Button';
+export { default as Header } from './Header';
+export { default as Categories } from './Categories';
+export { default as SortPopup } from './SortPopup';
+export { default as PizzaBlock } from './PizzaBlock';
