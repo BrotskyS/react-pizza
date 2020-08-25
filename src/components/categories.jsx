fe16de4368515be20bad0fc38651087ee165ca2b
Sergiy Brotsky <sergiybrotsky@MacBook-Pro-Sergiy.local>
2020-08-25 08:02:21 +0300

    Добавив катеогорії

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,63 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-// class Categories extends React.Component {
-//   state = {
-//     activeItem: 3,
-//     test: 123,
-//   };
+function Categories({items, onClickItem }) {
 
-//   onSelectItem = (index) => {
-//     this.setState({
-//       activeItem: index,
-//     });
-//   };
-
-//   render() {
-//     const { items, onClickItem } = this.props;
-//     console.log(this.state);
-//     return (
-//       <div className="categories">
-//         <ul>
-//           <li>Все</li>
-//           {items.map((name, index) => (
-//             <li
-//               className={this.state.activeItem === index ? 'active' : ''}
-//               onClick={() => this.onSelectItem(index)}
-//               key={`${name}_${index}`}>
-//               {name}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-function Categories({ items, onClickItem }) {
-  const [activeItem, setActiveItem] = React.useState(null);
-
-  const onSelectItem = (index) => {
-    setActiveItem(index);
-  };
-
+  const [activeItem, setActiveItem] = React.useState(null)
   return (
     <div className="categories">
-      <ul>
-        <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>
-          Все
-        </li>
-        {items &&
-          items.map((name, index) => (
-            <li
-              className={activeItem === index ? 'active' : ''}
-              onClick={() => onSelectItem(index)}
-              key={`${name}_${index}`}>
-              {name}
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
+          <ul>
+            <li className={activeItem === null ? 'active' : ''} onClick={()=> setActiveItem(null)}>Все</li>
+            
+            { items&& items.map((name, index) => (
+              <li 
+                className={activeItem === index ? 'active' : ''}
+                onClick={() => setActiveItem(index)} 
+                key={`${name}_${index}`}>{name}</li>
+                
+            ))}
+          </ul>
+      </div>
+  )
 }
 
-export default Categories;
+export default Categories
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import './scss/app.scss';
 
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
   document.getElementById('root'),
 );
