c03f349999489b1476ac939f141520ae8ae95ac9
Sergiy Brotsky <sergiybrotsky@MacBook-Pro-Sergiy.local>
2020-09-01 15:49:24 +0300

    Добавив компоненти піци

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {Header} from './components/index'
 import {Home, Cart} from './pages'
-import { Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import axios from 'axios'
 function App() {
+    const [pizzas, setPizzas] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then(({data}) => {
+            setPizzas(data.pizzas)
+        })
+    }, [])
   return (
     <div className="wrapper">
       <Header />
-      <Route path='/' component={Home} exact/>
+      <Route path='/' render={() => <Home items={pizzas}/>} exact/>
       <Route path='/cart' component={Cart} exact/>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Buttom from './Button'
 import Categories from './Categories'
 import Header from './Header'
 import SortPopup from './SortPopup'
+import PizzaBlock from "./PizzaBlock";
 
 
-export { Buttom, Categories, Header, SortPopup}
+export { Buttom, Categories, Header, SortPopup, PizzaBlock}
